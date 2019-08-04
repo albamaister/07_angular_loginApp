95d4629e0445e7323498281aac1c16c29920988e
Andres Alba <bryanalba92@gmail.com>
2019-08-04 18:47:45 -0500

    Mostrar notificaciones al usuario con sweetalert2 y redireccionar a home

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UsuarioModel } from '../../models/usuario.model';
 import { AuthService } from '../../services/auth.service';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,16 +12,32 @@ import { AuthService } from '../../services/auth.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor( private auth: AuthService) { }
+  constructor( private auth: AuthService,
+               private router: Router ) { }
   usuario: UsuarioModel = new UsuarioModel();
   ngOnInit() {
   }
   login( form: NgForm ) {
     if ( form.invalid ) { return; }
+    Swal.fire({
+      allowOutsideClick: false, // para prevenir que la persona pueda cerrar el clickAlert si hace click afuera
+      type: 'info',
+      text: 'Espere por favor...'
+    });
+
+    Swal.showLoading();
+
     this.auth.login( this.usuario ).subscribe( resp => {
       console.log(resp);
+      Swal.close();
+      this.router.navigateByUrl('/home');
     }, (err) => {
       console.log(err.error.error.message);
+      Swal.fire({
+        type: 'error',
+        title: 'Error al autenticar',
+        text: err.error.error.message
+      });
     });
   }
 
--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UsuarioModel } from '../../models/usuario.model';
 import { AuthService } from '../../services/auth.service';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
@@ -11,18 +13,32 @@ import { AuthService } from '../../services/auth.service';
 export class RegistroComponent implements OnInit {
 
   usuario: UsuarioModel;
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService,
+              private router: Router) { }
 
   ngOnInit() {
     this.usuario = new UsuarioModel();
    }
 
    onSubmit(form: NgForm) {
-     if( form.invalid ) { return; }
+     if ( form.invalid ) { return; }
+     Swal.fire({
+      allowOutsideClick: false, // para prevenir que la persona pueda cerrar el clickAlert si hace click afuera
+      type: 'info',
+      text: 'Espere por favor...'
+    });
+
      this.auth.nuevoUsuario(this.usuario).subscribe(resp => {
+      Swal.close();
+      this.router.navigateByUrl('/home');
       console.log(resp);
      }, (err) => {
        console.log(err.error.error.message);
+       Swal.fire({
+        type: 'error',
+        title: 'Error al autenticar',
+        text: err.error.error.message
+      });
      });
    }
 
